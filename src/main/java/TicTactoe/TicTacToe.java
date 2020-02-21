49c28e545548593b8994748a097ac9a1f7726e6a
KaczmarekIgor <Igor009@interia.eu>
2020-02-21 18:06:00 +0100

    done two methods

--- src/main/java/TicTactoe/TicTacToe.java
+++ src/main/java/TicTactoe/TicTacToe.java
@@ -3,11 +3,15 @@ package TicTactoe;
 import java.util.Random;
 import java.util.Scanner;
 
+import static TicTactoe.gameBoard.fillGameBoard;
 import static TicTactoe.gameBoard.printGameBoard;
 
 public class TicTacToe {
 
     public static void main(String[] args) {
+        // Print only gameBoard
+        fillGameBoard();
+        // gameBoard which contains numbers
         printGameBoard();
 
 
--- src/main/java/TicTactoe/gameBoard.java
+++ src/main/java/TicTactoe/gameBoard.java
@@ -1,6 +1,8 @@
 package TicTactoe;
 
-public  class gameBoard {
+import java.util.Scanner;
+
+public class gameBoard {
     static char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '},
@@ -15,10 +17,41 @@ public  class gameBoard {
             System.out.println();
         }
     }
+
+
+    public static void fillGameBoard() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number 1-9");
+        int number = scanner.nextInt();
+        switch (number) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
+    }
 }
-
-
-
-
-
 
