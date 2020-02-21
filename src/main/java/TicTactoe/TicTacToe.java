a5c3bd2d5cd4e94e91b17ece8da249dc3a8052cb
KaczmarekIgor <Igor009@interia.eu>
2020-02-21 17:07:59 +0100

    Declaration row, field, column

--- src/main/java/TicTactoe/TicTacToe.java
+++ src/main/java/TicTactoe/TicTacToe.java
@@ -1,66 +1,23 @@
 package TicTactoe;
 
+import java.util.Random;
 import java.util.Scanner;
 
+import static TicTactoe.gameBoard.printGameBoard;
+
 public class TicTacToe {
+
     public static void main(String[] args) {
+        printGameBoard();
 
-        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '}};
 
-        printGameBoard(gameBoard);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your placement (1-9)");
-        int position = scanner.nextInt();
-        System.out.println(position);
-        switch (position) {
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
-            case 2:
-                gameBoard[0][1] = 'X';
-                break;
-            case 3:
-                gameBoard[0][2] = 'X';
-                break;
-            case 4:
-                gameBoard[1][0] = 'X';
-                break;
-            case 5:
-                gameBoard[1][1] = 'X';
-                break;
-            case 6:
-                gameBoard[1][2] = 'X';
-                break;
-            case 7:
-                gameBoard[2][0] = 'X';
-                break;
-            case 8:
-                gameBoard[2][1] = 'X';
-                break;
-            case 9:
-                gameBoard[2][2] = 'X';
-                break;
-        }
-    }
 
-    public static void printGameBoard(char[][] gameBoard) {
-        for (char[] row : gameBoard) {
-            for (char c : row
-            ) {
-                System.out.print(c);
-            }
-            System.out.println();
-        }
+
+
+
 
 
     }
+
+
 }
-
-
-
-
-
